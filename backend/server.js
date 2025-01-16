94debb553e64f09507c3dd9133940b3a4fe8feec
Bahaeeddine27 <kiliminelbooking@gmail.com>
2025-01-16 14:39:37 +0100

    Coupon Routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(5000, () =>{
     console.log("Server is running on http://localhost:" + PORT);
