8f50cb0ad741f7fd1ab632d00e0fc71902c32a90
Bahaeeddine27 <kiliminelbooking@gmail.com>
2025-01-16 14:09:58 +0100

    Cart Routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
@@ -13,8 +14,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.listen(5000, () =>{
     console.log("Server is running on http://localhost:" + PORT);
